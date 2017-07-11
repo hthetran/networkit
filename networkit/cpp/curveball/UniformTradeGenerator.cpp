22e2d0da05cbed94fdd55f65190c88da32b6a50b
hungthetran <thehung@hotmail.de>
2017-07-11 23:36:03 +0200

    Added cpp file

---
+++ networkit/cpp/curveball/UniformTradeGenerator.cpp
@@ -0,0 +1,36 @@
+/*
+ * UniformTradeGenerator.cpp
+ *
+ *  Created on: Jul 11, 2017
+ *  	Author: Hung Tran
+ */
+
+#include "UniformTradeGenerator.h"
+#include "../auxiliary/Random.h"
+
+namespace CurveBall {
+	using value_type = std::vector<TradeDescriptor>;
+	using node_vector = std::vector<node_t>;
+
+	UniformTradeGenerator::UniformTradeGenerator(const int_t trade_num, const node_t num_nodes)
+		: _num_nodes(num_nodes)
+		, _trade_num(trade_num)
+	{ }
+
+	const value_type& UniformTradeGenerator::generate() {
+		_trades_out.clear();
+
+		for (tradeid_t t_id = 0; t_id < _trade_num; t_id++) {
+			const node_t fst = Aux::Random::integer(_num_nodes - 1);
+			while (true) {
+				const node_t snd = Aux::Random::integer(_num_nodes - 1);
+				if (fst != snd) {
+					_trades_out.push_back(TradeDescriptor(fst, snd));
+					break;
+				}
+			}
+		}
+
+		return _trades_out;
+	}
+}
