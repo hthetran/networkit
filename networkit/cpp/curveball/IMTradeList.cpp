fd0c6a4c63384c3e043378194df317e4cb2ac64d
hungthetran <thehung@hotmail.de>
2017-07-11 12:03:43 +0200

    Removed sorting, instead 2 scans

--- networkit/cpp/curveball/IMTradeList.cpp
+++ networkit/cpp/curveball/IMTradeList.cpp
@@ -18,46 +18,53 @@ IMTradeList::IMTradeList(const trade_vector& trades, const node_t num_nodes)
 	assert(trades.size() > 0);
 
 	std::vector<std::pair<node_t, tradeid_t> > occures;
+	std::vector<tradeid_t> trade_count(num_nodes);
 
 	// Push occurences
-	tradeid_t trade_id = 0;
-	for (auto t_it = trades.begin(); t_it != trades.end(); t_it++, trade_id++) {
+	for (auto t_it = trades.cbegin(); t_it != trades.cend(); t_it++) {
 		assert((*t_it).fst() >= 0);
 		assert((*t_it).fst() < num_nodes);
 		assert((*t_it).snd() >= 0);
 		assert((*t_it).snd() < num_nodes);
-		occures.push_back(std::pair<node_t, tradeid_t>((*t_it).fst(), trade_id));
-		occures.push_back(std::pair<node_t, tradeid_t>((*t_it).snd(), trade_id));
+
+		trade_count[(*t_it).fst()]++;
+		trade_count[(*t_it).snd()]++;
 	}
+	
+	// calc prefix sums...
+	auto count_it = trade_count.begin();
+	tradeid_t pre_sum = 0;
+	node_t curr_node = 1;
 
-	// Sort occurences, to get sorted list of node -> trade correspondence
-	std::sort(occures.begin(), occures.end());
+	do {
+		assert(curr_node < num_nodes);
+		_offsets[curr_node] = pre_sum + *count_it + 1;
+		pre_sum = _offsets[curr_node];
+		_trade_list[_offsets[curr_node] - 1] = TRADELIST_END;
+		count_it++;
+		curr_node++;
+	} while (curr_node < num_nodes);
+	
+	// set last entry as sentinel
+	_trade_list.back() = TRADELIST_END;
 
-	// Fill helper datastructure	
-	auto occ_it = occures.begin();
-	node_t nodeid = 0;
-	node_t prev = INVALID_NODE;
-	for (tradeid_t pos = 0; pos < static_cast<tradeid_t>(_trade_list.size()); pos++) {
-		auto node_trade = *occ_it;
-		// no more node_trade pairs or at node with no trades
-		if ((occ_it == occures.end()) || (nodeid < node_trade.first)) {
-			// if node with no trades, only then set offset
-			// nodes with trades are handled below, and get their offset there already
-			if (prev != nodeid)
-				_offsets[nodeid] = pos;
-			_trade_list[pos] = TRADELIST_END;
-			prev = nodeid;
-			nodeid++;
-			continue;
-		} 
-		if (nodeid == node_trade.first) {
-			if (prev != nodeid)
-				_offsets[nodeid] = pos;
-			_trade_list[pos] = node_trade.second;
-			occ_it++;
-		}
+	std::vector<tradeid_t> tmp_counter(num_nodes);
 
-		prev = nodeid;
+	tradeid_t trade_id = 0;
+	for (auto t_it = trades.begin(); t_it != trades.end(); t_it++, trade_id++) {
+		// process first node
+		const node_t fst_node = (*t_it).fst();
+
+		const node_t fst_pos = _offsets[fst_node] + tmp_counter[fst_node];
+		_trade_list[fst_pos] = trade_id;
+		tmp_counter[fst_node]++;
+
+		// process second node
+		const node_t snd_node = (*t_it).snd();
+
+		const node_t snd_pos = _offsets[snd_node] + tmp_counter[snd_node];
+		_trade_list[snd_pos] = trade_id;
+		tmp_counter[snd_node]++;
 	}
 }
 
