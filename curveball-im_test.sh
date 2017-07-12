3523aa35a4ead531bb1051a22b84da31b07940f1
Hung Tran <thehung@hotmail.de>
2017-07-12 17:05:26 +0200

    Added if conditional for successful builds

--- curveball-im_test.sh
+++ curveball-im_test.sh
@@ -1,2 +1,5 @@
-scons --optimize=Dbg --target=Tests
-./NetworKit-Tests-Dbg --gtest_filter="IM*":"*Trade*"
+if scons --optimize=Dbg --target=Tests ; then
+    ./NetworKit-Tests-Dbg --gtest_filter="IM*":"*Trade*"
+else
+    echo "Build unsuccessful." 
+fi
