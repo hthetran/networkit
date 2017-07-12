7c6dc9f526360cb2f6efa0e3dc8ab13de2872599
Hung Tran <thehung@hotmail.de>
2017-07-12 17:08:25 +0200

    Added Curveball algorithm skeleton

---
+++ networkit/cpp/curveball/Curveball.cpp
@@ -0,0 +1,23 @@
+/*
+ * Curveball.cpp
+ *
+ *  Created on: Jul 12 2017
+ *      Author: Hung Tran
+ */
+
+#include "Curveball.h"
+#include "../graph/Graph.h"
+
+namespace CurveBall {
+
+    Curveball::Curveball(const NetworKit::Graph& G)
+        : _G(G)
+    {
+
+    }
+
+   //NetworKit::Graph& CurveballN::getGraph() const {
+
+//   }
+
+}
