3d925404c2c7ec8c501b48939baa1e80224e473e
Hung Tran <hung@ae.cs.uni-frankfurt.de>
2017-07-15 16:58:46 +0200

    Fixed Bug, Curveball now tested too

--- curveball-im_test.sh
+++ curveball-im_test.sh
@@ -1,6 +1,6 @@
 if scons --optimize=Dbg --target=Tests ; then
     # We try testing in a topological order
-	./NetworKit-Tests-Dbg --gtest_filter="Trade*":"IM*":"GlobalTrade*":"UniformTrade*":"Curveball"
+	./NetworKit-Tests-Dbg --gtest_filter="Trade*":"IM*":"GlobalTrade*":"UniformTrade*":"Curveball*"
 else
     echo "Build unsuccessful." 
 fi
