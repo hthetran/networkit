49ffb009217a5336c1cef6a2a79da0c99ec8bb21
Hung Tran <hung@ae.cs.uni-frankfurt.de>
2017-07-18 13:46:10 +0200

    Added new GraphBuilder skeleton

---
+++ networkit/cpp/curveball/IMAdjacencyListMaterialization.cpp
@@ -0,0 +1,16 @@
+/*
+ * IMAdjacencyListMaterialization.cpp
+ *
+ *  Created on: Jul 18, 2017
+ *      Author: Hung Tran
+ */
+#include "IMAdjacencyListMaterialization.h"
+
+namespace CurveBall {
+    
+    NetworKit::Graph IMAdjacencyListMaterialization::materialize(const IMAdjacencyList& adj_list) const {
+        //TODO
+        return NetworKit::Graph(5);
+    }
+
+}
