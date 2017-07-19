4e3a8127c6c2b5faa7cb714125977fb51522e03d
Hung Tran <hung@ae.cs.uni-frankfurt.de>
2017-07-19 20:16:33 +0200

    Added Test skeleton for new builder

---
+++ networkit/cpp/curveball/test/IMAdjacencyListMaterializationGTest.cpp
@@ -0,0 +1,19 @@
+/*
+ * IMAdjacencyListMaterializationGTest.cpp
+ *
+ *  Created on: 19 Jul, 2017
+ *	Author: Hung Tran
+ */
+
+#include "IMAdjacencyListMaterializationGTest.h"
+#include "../IMAdjacencyListMaterialization.h"
+#include "../../graph/GraphBuilder.h"
+#include "../../graph/Graph.h"
+
+namespace CurveBall {
+
+TEST_F(IMAdjacencyListMaterializationGTest, testNewBuilder) {
+	
+}
+
+}
---
+++ networkit/cpp/curveball/test/IMAdjacencyListMaterializationGTest.h
@@ -0,0 +1,22 @@
+/**
+ * IMAdjacencyListMaterializationGTest.h
+ *
+ *  Created on: Jul 19, 2017
+ *	Author: Hung Tran
+ */
+
+#ifndef CB_IMADJACENCYLISTMATERIALIZATION_H
+#define CB_IMADJACENCYLISTMATERIALIZATION_H
+
+#include <gtest/gtest.h>
+
+namespace CurveBall {
+
+	class IMAdjacencyListMaterializationGTest : public testing::Test {
+	public:
+		IMAdjacencyListMaterializationGTest() = default;
+		virtual ~IMAdjacencyListMaterializationGTest() = default;
+	};
+}
+
+#endif//CB_IMADJACENCYLISTMATERIALIZATION_H
