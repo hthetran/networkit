f01e8a2e2fed9163327d7c69378169574124b76f
Hung Tran <hung@ae.cs.uni-frankfurt.de>
2017-07-20 23:36:31 +0200

    Added btree_map in the implementation

--- networkit/cpp/curveball/EdgeSwitchingMarkovChainRandomization.cpp
+++ networkit/cpp/curveball/EdgeSwitchingMarkovChainRandomization.cpp
@@ -10,8 +10,20 @@
 
 namespace CurveBall {
 
-	EdgeSwitchingMarkovChainRandomization::EdgeSwitchingMarkovChainRandomization(const NetworKit::Graph& G) {
-		const auto edges = G.edges();
+	using edgeswap_vector = std::vector< std::pair<edgeid_t, edgeid_t> >;
+
+	EdgeSwitchingMarkovChainRandomization::EdgeSwitchingMarkovChainRandomization(const NetworKit::Graph& G) 
+		: _edges(G.edges())
+	{
+		edgeid_t id = 0;
+		for (const edge_t edge : _edges) {
+			_edges_map.insert(std::make_pair(id, edge));
+			id++;
+		}
 	}
 
+	void EdgeSwitchingMarkovChainRandomization::run(const edgeswap_vector& swaps) {
+		// TODO: get edges by find(id's)
+		// TODO: search the swapped by find again	
+	}
 }
