ed6cdce32c1d762fa4d6d084ea0a625337b5f0eb
Hung Tran <hung@ae.cs.uni-frankfurt.de>
2017-07-21 00:28:06 +0200

    Declared swapped edges

--- networkit/cpp/curveball/EdgeSwitchingMarkovChainRandomization.cpp
+++ networkit/cpp/curveball/EdgeSwitchingMarkovChainRandomization.cpp
@@ -11,6 +11,7 @@
 namespace CurveBall {
 
 	using edgeswap_vector = std::vector< std::pair<edgeid_t, edgeid_t> >;
+	using nodepair = std::pair<node_t, node_t>;
 
 	EdgeSwitchingMarkovChainRandomization::EdgeSwitchingMarkovChainRandomization(const NetworKit::Graph& G) 
 		: _edges(G.edges())
@@ -24,6 +25,28 @@ namespace CurveBall {
 
 	void EdgeSwitchingMarkovChainRandomization::run(const edgeswap_vector& swaps) {
 		// TODO: get edges by find(id's)
-		// TODO: search the swapped by find again	
+		// TODO: search the swapped by find again
+		for (const auto swap : swaps) {
+			// ids of edges to swap
+			const edgeid_t a = swap.first;
+			const edgeid_t b = swap.second;
+
+			// edge (iterators) with these ids
+			auto edgeit_a = _edges_map.find(a);
+			auto edgeit_b = _edges_map.find(b);
+
+			// edges
+			const nodepair edge_a = (*edgeit_a).second;
+			const nodepair edge_b = (*edgeit_b).second;
+
+			// swap direction
+			if (Aux::Random::integer(1)) {
+				const nodepair swapped_a = std::make_pair(edge_a.first, edge_b.first);
+				const nodepair swapped_b = std::make_pair(edge_a.second, edge_b.second);	
+			} else {
+				const nodepair swapped_a = std::make_pair(edge_a.first, edge_b.second);
+				const nodepair swapped_b = std::make_pair(edge_a.second, edge_b.first);
+			}
+		}
 	}
 }
