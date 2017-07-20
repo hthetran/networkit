8d482bb88bd71067a27857751dda4999e1209259
Hung Tran <hung@ae.cs.uni-frankfurt.de>
2017-07-20 15:45:52 +0200

    Added EdgeSwitchingMarkovChainRandomization skeleton

---
+++ networkit/cpp/curveball/EdgeSwitchingMarkovChainRandomization.cpp
@@ -0,0 +1,17 @@
+/*
+ * EdgeSwitchingMarkovChainRandomization.cpp
+ *
+ *  Created on: Jul 20, 2017
+ *	Author: Hung Tran
+ */
+
+#include "EdgeSwitchingMarkovChainRandomization.h"
+#include "../auxiliary/Random.h"
+
+namespace CurveBall {
+
+	EdgeSwitchingMarkovChainRandomization::EdgeSwitchingMarkovChainRandomization(const NetworKit::Graph& G) {
+
+	}
+
+}
