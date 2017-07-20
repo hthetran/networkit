3cf3684aaab985b77b2ec30ce77708f51475f0aa
Hung Tran <hung@ae.cs.uni-frankfurt.de>
2017-07-20 23:35:48 +0200

    Added ESMC-Randomization test

--- curveball-im_test.sh
+++ curveball-im_test.sh
@@ -1,6 +1,6 @@
 if scons --optimize=Dbg --target=Tests --jobs=8 ; then
     # We try testing in a topological order
-	./NetworKit-Tests-Dbg --gtest_filter="Trade*":"IM*":"GlobalTrade*":"UniformTrade*":"Curveball*"
+	./NetworKit-Tests-Dbg --gtest_filter="Trade*":"IM*":"GlobalTrade*":"UniformTrade*":"Curveball*":"EdgeSwitchingMarkovChainRandomization*"
 else
     echo "Build unsuccessful." 
 fi
