b3bf1a11ccfdf7bc2a9aa70be9ca51b02829a83c
Hung Tran <hung@ae.cs.uni-frankfurt.de>
2017-07-20 23:36:31 +0200

    Added btree_map in the implementation

--- networkit/cpp/curveball/EdgeSwitchingMarkovChainRandomization.h
+++ networkit/cpp/curveball/EdgeSwitchingMarkovChainRandomization.h
@@ -12,20 +12,30 @@
 #include "../base/Algorithm.h"
 #include "../graph/Graph.h"
 #include "defs.h"
-#include "stx/btree_map.h"
+#include <stx/btree_map.h>
 
 namespace CurveBall {
 
 using edgeswap_vector = std::vector< std::pair<edgeid_t, edgeid_t> >;
+using edge_vector = std::vector< std::pair<node_t, node_t> >;
 
 class EdgeSwitchingMarkovChainRandomization : public NetworKit::Algorithm {
 
+protected:
+	edge_vector _edges;
+	stx::btree_map<edgeid_t, std::pair<node_t, node_t> > _edges_map;
+
 public:
 	EdgeSwitchingMarkovChainRandomization(const NetworKit::Graph& G);
-	
+
+	void run() {
+		std::runtime_error("Cannot use this method, algorithm needs to receive swaps");
+	}
+
 	void run(const edgeswap_vector& swaps);
 
 	// is const return necessary?
+	// TODO: EdgeVectorMaterialization
 	NetworKit::Graph getGraph();
 };
 
