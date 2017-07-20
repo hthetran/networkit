23ead96eb6afa24c1a04e8e2d9699e61f72496aa
Hung Tran <hung@ae.cs.uni-frankfurt.de>
2017-07-20 12:46:02 +0200

    Curveball algorithm now works in python

--- networkit/_NetworKit.pyx
+++ networkit/_NetworKit.pyx
@@ -2452,13 +2452,11 @@ cdef class Curveball(Algorithm):
 		if isinstance(G, Graph):
 			self._this = new _Curveball((<Graph>G)._this)
 
-	@classmethod
 	def run(self, vector[pair[node, node]] trades):
 		with nogil:	
 			(<_Curveball*>(self._this)).run(trades)
 		return self
 
-	@classmethod
 	def getGraph(self):
 		return Graph().setThis((<_Curveball*>self._this).getGraph())
 
