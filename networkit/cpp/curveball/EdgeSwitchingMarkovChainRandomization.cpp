e66954051d19979c26b01d7cbc57ff9bbf5a4600
Hung Tran <hung@ae.cs.uni-frankfurt.de>
2017-07-21 11:32:05 +0200

    Commiting state that should build

--- networkit/cpp/curveball/EdgeSwitchingMarkovChainRandomization.cpp
+++ networkit/cpp/curveball/EdgeSwitchingMarkovChainRandomization.cpp
@@ -11,14 +11,14 @@
 namespace CurveBall {
 
 	using edgeswap_vector = std::vector< std::pair<edgeid_t, edgeid_t> >;
-	using nodepair = std::pair<node_t, node_t>;
 
 	EdgeSwitchingMarkovChainRandomization::EdgeSwitchingMarkovChainRandomization(const NetworKit::Graph& G) 
 		: _edges(G.edges())
 	{
 		edgeid_t id = 0;
 		for (const edge_t edge : _edges) {
-			_edges_map.insert(std::make_pair(id, edge));
+			// Conversion from networkit's edges to ours
+			_edgeid_map.insert(std::make_pair((edge_t) edge, id));
 			id++;
 		}
 	}
@@ -27,26 +27,13 @@ namespace CurveBall {
 		// TODO: get edges by find(id's)
 		// TODO: search the swapped by find again
 		for (const auto swap : swaps) {
-			// ids of edges to swap
-			const edgeid_t a = swap.first;
-			const edgeid_t b = swap.second;
-
-			// edge (iterators) with these ids
-			auto edgeit_a = _edges_map.find(a);
-			auto edgeit_b = _edges_map.find(b);
-
-			// edges
-			const nodepair edge_a = (*edgeit_a).second;
-			const nodepair edge_b = (*edgeit_b).second;
-
-			// swap direction
-			if (Aux::Random::integer(1)) {
-				const nodepair swapped_a = std::make_pair(edge_a.first, edge_b.first);
-				const nodepair swapped_b = std::make_pair(edge_a.second, edge_b.second);	
-			} else {
-				const nodepair swapped_a = std::make_pair(edge_a.first, edge_b.second);
-				const nodepair swapped_b = std::make_pair(edge_a.second, edge_b.first);
-			}
+			const edge_t edge_a = _edges[swap.first];
+			const edge_t edge_b = _edges[swap.second];
+			
+			// swapped edges
+			const bool dir = (bool) Aux::Random::integer(1);
+			const edge_t swapped_a = (dir ? edge_t{edge_a.first, edge_b.first} : edge_t{edge_a.first, edge_b.second}); 
+			const edge_t swapped_b = (dir ? edge_t{edge_a.second, edge_b.second} : edge_t{edge_a.second, edge_b.first}); 
 		}
 	}
 }
