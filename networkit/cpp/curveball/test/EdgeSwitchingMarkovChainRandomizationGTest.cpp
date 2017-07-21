b39f46c03ac06f039a85168f093ae26ede4716ba
Hung Tran <hung@ae.cs.uni-frankfurt.de>
2017-07-21 15:56:08 +0200

    Added ESMC test

--- networkit/cpp/curveball/test/EdgeSwitchingMarkovChainRandomizationGTest.cpp
+++ networkit/cpp/curveball/test/EdgeSwitchingMarkovChainRandomizationGTest.cpp
@@ -11,12 +11,18 @@
 
 namespace CurveBall {
 
-TEST_F(EdgeSwitchingMarkovChainRandomizationGTest, testRun) {
+using edgeswap_vector = std::vector< std::pair<edgeid_t, edgeid_t> >;
+
+TEST_F(EdgeSwitchingMarkovChainRandomizationGTest, testRunSingleTrade) {
 	NetworKit::Graph tG(4);
 	tG.addEdge(0, 1);
 	tG.addEdge(2, 3);
 
 	EdgeSwitchingMarkovChainRandomization algo(tG);
+	
+	const edgeswap_vector swaps = { std::make_pair(0, 1) };
+
+	algo.run(swaps);
 }
 
 }
