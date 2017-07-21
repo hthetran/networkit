10698a86253ece62eaf6c0dbf6ced52015389334
Hung Tran <hung@ae.cs.uni-frankfurt.de>
2017-07-21 15:57:23 +0200

    Added Materialization from EdgeVector skeleton

---
+++ networkit/cpp/curveball/EdgeVectorMaterialization.cpp
@@ -0,0 +1,16 @@
+/*
+ * EdgeVectorMaterialization.cpp
+ *
+ *  Created on: Jul 21, 2017
+ *	Author: Hung Tran
+ */
+
+#include "EdgeVectorMaterialization.h"
+
+namespace NetworKit {
+
+	NetworKit::Graph EdgeVectorMaterialization::materialize(const edge_vector& edges, const degree_vector& degrees) {
+
+	}
+
+}
