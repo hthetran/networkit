ee2dd3be47d7582da39b2a45fdecd5900f0d82f0
Hung Tran <hung@ae.cs.uni-frankfurt.de>
2017-07-22 15:04:28 +0200

    Implemented EdgeSwitchingMarkovChain randomization

--- networkit/cpp/curveball/EdgeSwitchingMarkovChainRandomization.cpp
+++ networkit/cpp/curveball/EdgeSwitchingMarkovChainRandomization.cpp
@@ -8,32 +8,26 @@
 #include "EdgeSwitchingMarkovChainRandomization.h"
 #include "../auxiliary/Random.h"
 #include "../graph/Graph.h"
-#include "EdgeVectorMaterialization.h"
 
 namespace CurveBall {
 
 	using edgeswap_vector = std::vector< std::pair<edgeid_t, edgeid_t> >;
 	
 	EdgeSwitchingMarkovChainRandomization::EdgeSwitchingMarkovChainRandomization(const NetworKit::Graph& G) 
-	{
-/*		_edges.reserve(G.numberOfEdges());
-		edgeid_t id = 0;
+		: _G(G)
+	{ 
+		_edges.reserve(G.numberOfEdges());
 		for (const auto edge : G.edges()){
-			_edges.push_back((edge_t) edge);
-			_edgeid_map.insert(std::make_pair((edge_t) edge, id));
-			id++;
+			_edges.push_back(static_cast<edge_t>(edge));
 		}
 
 		_degrees.reserve(G.numberOfNodes());
 		G.forNodes([&](node_t v) {
 			_degrees.push_back(G.degree(v));
-		});*/
+		});
 	}
 
 	void EdgeSwitchingMarkovChainRandomization::run(const edgeswap_vector& swaps) {
-		/*
-		// TODO: get edges by find(id's)
-		// TODO: search the swapped by find again
 		for (const auto swap : swaps) {
 			const edge_t edge_a = _edges[swap.first];
 			const edge_t edge_b = _edges[swap.second];
@@ -48,25 +42,22 @@ namespace CurveBall {
 				continue;
 			
 			// check for edge existence
-			if ((_edgeid_map.find(swapped_a) != _edgeid_map.end()) || (_edgeid_map.find(swapped_b) != _edgeid_map.end()))
+			if (_G.hasEdge(swapped_a.first, swapped_a.second) || _G.hasEdge(swapped_b.first, swapped_b.second))
 				continue;
 			else {
 				// update now old edges with swapped ones
-				_edgeid_map.erase(edge_a);
-				_edgeid_map.erase(edge_b);
-
+				// careful, since swapEdge swaps in a particular way
 				_edges[swap.first] = swapped_a;
 				_edges[swap.second] = swapped_b;
-
-				_edgeid_map.insert(std::make_pair(swapped_a, swap.first));
-				_edgeid_map.insert(std::make_pair(swapped_b, swap.second));
+				if (!dir)
+					_G.swapEdge(edge_a.first, edge_a.second, edge_b.first, edge_b.second);
+				else
+					_G.swapEdge(edge_a.second, edge_a.first, edge_b.first, edge_b.second);
 			}
-		}*/
+		}
 	}
 
 	NetworKit::Graph EdgeSwitchingMarkovChainRandomization::getGraph() const {
-		/*NetworKit::EdgeVectorMaterialization gb;
-		return gb.materialize(_edges, _degrees);*/
-		return NetworKit::Graph(5);
+		return _G;
 	}
 }
