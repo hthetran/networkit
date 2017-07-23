c7d8bf302db4e56174407203bc6ca951280de4e3
Hung Tran <hung@ae.cs.uni-frankfurt.de>
2017-07-23 19:55:39 +0200

    Removed computation of degrees

--- networkit/cpp/curveball/EdgeSwitchingMarkovChainRandomization.h
+++ networkit/cpp/curveball/EdgeSwitchingMarkovChainRandomization.h
@@ -23,7 +23,6 @@ class EdgeSwitchingMarkovChainRandomization : public NetworKit::Algorithm {
 
 protected:
 	edge_vector _edges;
-	degree_vector _degrees;
 	// maybe reference? change given graph, or new one
 	NetworKit::Graph _G;
 
