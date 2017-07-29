36aef454657bd9cf1decee89ec738c366ccd6ced
Hung Tran <hung@ae.cs.uni-frankfurt.de>
2017-07-29 15:01:51 +0200

    Added experiment scripts for the Power (MEJ.Newman) graph

--- experiments/power_autocorr.py
+++ experiments/power_autocorr.py
@@ -1,22 +1,34 @@
+#!/usr/bin/env python3
+
 from networkit import *
 import copy
+import timeit
+import os
 
-print("Imported modules")
+path = os.path.dirname(os.path.realpath(__file__))
+f = open(path + "/out.log", 'w')
 
-print("Importing Power")
+class Walltime:
+	def __init__(self, label, f):
+		self.label = label
+
+	def __enter__(self):
+		self.start_time = timeit.default_timer()
+
+	def __exit__(self, a, b, c):
+		f.write("%s: Runtime %f s" % (self.label, timeit.default_timer() - self.start_time))
+
 gPower = graphio.readGraph("power.gml", Format.GML)
 for u, v in gPower.edges():
-    gPower.addEdge(v, u)
+	gPower.addEdge(v, u)
 
-print("Checking autocorrelation of time-series")
-print("eps: 4.5e-3 (10*m)")
 gTimeSeries = copy.deepcopy(gPower)
 swapGen = curveball.UniformTradeGenerator(gPower.numberOfEdges(), gPower.numberOfEdges())
 esmc = curveball.EdgeSwitchingMarkovChainRandomization(gTimeSeries)
 aa = curveball.AutocorrelationAnalysis(1000)
-for i in range(1000):#range(30000*gPower.numberOfEdges()):
-    print(i)
-    esmc.run(swapGen.generate())
-    aa.addSample(esmc.getGraph())
+with Walltime("Power", f):
+	for i in range(1000):#range(30000*gPower.numberOfEdges()):
+		esmc.run(swapGen.generate())
+		aa.addSample(esmc.getGraph())
 
-print("Done.")
+f.close()
