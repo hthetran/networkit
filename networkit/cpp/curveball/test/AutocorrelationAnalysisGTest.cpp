9d1a07fec8ec93a3833ea398e019e5e54e74d898
Hung Tran <hung@ae.cs.uni-frankfurt.de>
2017-08-26 13:57:00 +0200

    Implemented time-series analysis directly in c++

--- networkit/cpp/curveball/test/AutocorrelationAnalysisGTest.cpp
+++ networkit/cpp/curveball/test/AutocorrelationAnalysisGTest.cpp
@@ -7,13 +7,16 @@
 
 #include "AutocorrelationAnalysisGTest.h"
 #include "../AutocorrelationAnalysis.h"
+#include "../EdgeSwitchingMarkovChainRandomization.h"
+#include "../UniformTradeGenerator.h"
 #include "../../graph/Graph.h"
+#include "../../generators/ErdosRenyiGenerator.h"
 
 namespace CurveBall {
 
 	TEST_F(AutocorrelationAnalysisGTest, testGraphInput) {
 		AutocorrelationAnalysis aa(3);
-		
+
 		NetworKit::Graph G(5);
 		G.addEdge(0, 1);
 		G.addEdge(3, 4);
@@ -46,7 +49,7 @@ namespace CurveBall {
 		ASSERT_FALSE((edgeExistences2[1].second)[0]);
 		ASSERT_TRUE((edgeExistences2[1].second)[1]);
 		ASSERT_FALSE((edgeExistences2[1].second)[2]);
-		
+
 		G.addEdge(0, 1);
 
 		aa.addSample(G);
@@ -57,10 +60,10 @@ namespace CurveBall {
 		ASSERT_FALSE((edgeExistences3[0].second)[1]);
 		ASSERT_TRUE((edgeExistences3[0].second)[2]);
 	}
-	
+
 	TEST_F(AutocorrelationAnalysisGTest, testEdgeVectorInput) {
 		AutocorrelationAnalysis aa(3);
-		
+
 		NetworKit::Graph G(5);
 		G.addEdge(0, 1);
 		G.addEdge(3, 4);
@@ -93,7 +96,7 @@ namespace CurveBall {
 		ASSERT_FALSE((edgeExistences2[1].second)[0]);
 		ASSERT_TRUE((edgeExistences2[1].second)[1]);
 		ASSERT_FALSE((edgeExistences2[1].second)[2]);
-		
+
 		G.addEdge(0, 1);
 
 		aa.addSample(G.edges());
@@ -105,5 +108,24 @@ namespace CurveBall {
 		ASSERT_TRUE((edgeExistences3[0].second)[2]);
 	}
 
+	TEST_F(AutocorrelationAnalysisGTest, testLogLinearEstimate) {
+		NetworKit::ErdosRenyiGenerator gen(50, 0.5);
+		NetworKit::Graph G = gen.generate();
+		EdgeSwitchingMarkovChainRandomization esmc(G);
+		UniformTradeGenerator swaps(G.numberOfEdges()/10, G.numberOfEdges());
+		const NetworKit::count run_length = 101;
 
+		const std::vector<NetworKit::count> thinnings = {1, 2, 4, 8, 16, 32};//, 16, 32, 64, 128, 256};
+		const NetworKit::count max_thinning = 32;
+		const NetworKit::count chain_length = run_length * max_thinning;
+
+		AutocorrelationAnalysis aa(chain_length);
+
+		for (NetworKit::count count = 0; count < chain_length; count++) {
+			esmc.run(swaps.generate());
+			aa.addSample(esmc.getEdges());
+		}
+
+		aa.getIndependenceRate(thinnings, run_length);
+	}
 }
