889f97101ab4a39fda9f4340614b6bbcb2c74fea
Hung Tran <hung@ae.cs.uni-frankfurt.de>
2017-09-04 16:06:48 +0200

    Fixed whitespace and indentations

--- networkit/cpp/curveball/Curveball.h
+++ networkit/cpp/curveball/Curveball.h
@@ -17,63 +17,63 @@
 
 namespace CurveBall {
 
-    using trade_vector = std::vector<TradeDescriptor>;
+	using trade_vector = std::vector<TradeDescriptor>;
 
 	using nodepair_vector = std::vector< std::pair<node_t, node_t> >;
 
-    class Curveball : public NetworKit::Algorithm {
+	class Curveball : public NetworKit::Algorithm {
 
-    protected:
-        const NetworKit::Graph& _G;
-        const node_t _num_nodes;
-	IMAdjacencyList _adj_list;
-	IMTradeList _trade_list;
-	degree_t _max_degree;
-	edgeid_t _aff_edges; // affected half-edges
+		protected:
+			const NetworKit::Graph& _G;
+			const node_t _num_nodes;
+			IMAdjacencyList _adj_list;
+			IMTradeList _trade_list;
+			degree_t _max_degree;
+			edgeid_t _aff_edges; // affected half-edges
 
-        void load_from_graph(const trade_vector& trades);
+			void load_from_graph(const trade_vector& trades);
 
-        void restructure_graph(const trade_vector& trades);
+			void restructure_graph(const trade_vector& trades);
 
-		inline void update(const node_t a, const node_t b) {
-			const tradeid_t ta = *(_trade_list.get_trades(a));
-			const tradeid_t tb = *(_trade_list.get_trades(b));
-			if (ta < tb) {
-				_adj_list.insert_neighbour(a, b);
+			inline void update(const node_t a, const node_t b) {
+				const tradeid_t ta = *(_trade_list.get_trades(a));
+				const tradeid_t tb = *(_trade_list.get_trades(b));
+				if (ta < tb) {
+					_adj_list.insert_neighbour(a, b);
+					return;
+				}
+
+				if (ta > tb) {
+					_adj_list.insert_neighbour(b, a);
+					return;
+				}
+				// ta == tb
+				{
+					_adj_list.insert_neighbour(a, b);
+				}
+			}
+
+		public:
+			Curveball(const NetworKit::Graph& G);
+
+			void run() override {
+				std::runtime_error("Invalid use of algorithm, provide trades!");
+
 				return;
+			};
+
+			void run(const trade_vector& trades);
+
+			bool isParallel() const override {
+				return false;
 			}
 
-			if (ta > tb) {
-				_adj_list.insert_neighbour(b, a);
-				return;
-			}
-			// ta == tb
-			{
-				_adj_list.insert_neighbour(a, b);
-			}
-		}
+			edgeid_t getNumberOfAffectedEdges() const;
 
-    public:
-        Curveball(const NetworKit::Graph& G);
+			NetworKit::Graph getGraph() const;
 
-	void run() override {
-	    std::runtime_error("Invalid use of algorithm, provide trades!");
-
-            return;
-        };
-
-        void run(const trade_vector& trades);
-
-        bool isParallel() const override {
-		return false;
-        }
-
-	edgeid_t getNumberOfAffectedEdges() const;
-
-        NetworKit::Graph getGraph() const;
-    
-	nodepair_vector getEdges() const;
-    };
+			nodepair_vector getEdges() const;
+	};
 }
 
 #endif //CB_CURVEBALL_H
