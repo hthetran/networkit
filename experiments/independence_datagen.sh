92591a4524eda6414870e166afcfbabfa3adf304
Hung Tran <hung@ae.cs.uni-frankfurt.de>
2018-01-24 18:27:40 +0100

    fixed errors

--- experiments/independence_datagen.sh
+++ experiments/independence_datagen.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --partition=parallel
-#SBATCH --constraint="intel20|broadwell"
+#BATCH --partition=parallel
+#BATCH --constraint="intel20|broadwell"
 
-#BATCH --partition=test
+#SBATCH --partition=test
 
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=10
-#SBATCH --mem-per-cpu=3200
+#SBATCH --mem-per-cpu=5800
 #SBATCH --time=0:20:00
 #SBATCH --mail-type=END
 
@@ -22,4 +22,4 @@ cp ${BASH_SOURCE[0]} $dir/
 cd $dir
 env
 
-srun -o out_%j_%t ./autocorr_analysis2.py --pus 10 --thins 8 16 32 64 128 --runlength 1000 --nodes X --runs 10 --samples 10 -cg
+srun -o out_%j_%t ./autocorr_analysis2.py --pus 10 --thins 8 --runlength 1000 --nodes 200 --runs 10 --samples 1 -cg
