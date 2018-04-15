801873b775202121bc5c69e47594c9a84c061a5e
Manuel Penschuck <manuel@ae.cs.uni-frankfurt.de>
2018-04-15 13:37:03 +0200

    Minor optimization in GlobalTradeGenerator

--- networkit/cpp/curveball/GlobalTradeGenerator.cpp
+++ networkit/cpp/curveball/GlobalTradeGenerator.cpp
@@ -28,12 +28,14 @@ namespace CurveBall {
         value_type _trades_out;
         _trades_out.reserve(_run_length * _trades_per_run);
 
-        for (NetworKit::count run = 0; run < _run_length; run++) {
-            node_vector node_permutation;
-			for (node_t node_id = 0; node_id < _num_nodes; node_id++) {
-				node_permutation.push_back(node_id);
-			}
+        node_vector node_permutation(_num_nodes);
+        for (node_t node_id = 0; node_id < _num_nodes; node_id++) {
+            node_permutation[node_id] = node_id;
+        }
 
+        for (NetworKit::count run = 0; run < _run_length; run++) {
+            // shuffling a shuffled node_permutation is okay, no need
+            // to reinitialize it
             std::shuffle(node_permutation.begin(), node_permutation.end(),
                          Aux::Random::getURNG());
 
